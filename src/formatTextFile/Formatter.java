0c22529b563175b8745279077b9e4b6872006af9
17livincent <53451780+17livincent@users.noreply.github.com>
2019-12-04 12:40:12 -0700

    Update Formatter.java
    Added a method to check for errors associated with file names, and to see if the input file exists

--- src/formatTextFile/Formatter.java
+++ src/formatTextFile/Formatter.java
@@ -28,4 +28,33 @@ public class Formatter {
 		}
 	}
 
+	/*
+	 * @description: This method checks to see whether or not the associated input and output files
+	 * exist, if they're the same, and if they're of .txt.
+	 * @return: An integer describing the type of error that has occurred.
+	 */
+	public String checkInitialErrors(String inputFileName, String outputFileName) {
+		String errorMessage = "";
+		File scratch = new File(inputFileName);
+		// Check to see if the output file has the .txt suffix
+		if(outputFileName.indexOf(".txt", outputFileName.length()) == -1) {
+			outputFileName += ".txt";
+			errorMessage += "Output file is not specified as type txt.\n";
+		}
+		// Check to see if the input file has the .txt suffix
+		if(inputFileName.indexOf(".txt", inputFileName.length()) == -1) {
+			errorMessage += "Input file is not specified as type txt.\n";
+		}
+		// Check to see if the input file exists
+		if(!scratch.exists()) {
+			errorMessage += "A input file of this name is not found.\n";
+		}
+		if(inputFileName.equalsIgnoreCase(outputFileName)) {
+			errorMessage += "Output file name is the same as the input file name.\n";
+		}
+		// None of the conditions above are true. Return 0, meaning all is clear
+		
+		return errorMessage;
+		
+	
 }
